96f408760c9b7d95d0b9850132bce5d62db7edc1
Frederic Jacobs <freeric.jacobs@mac.com>
2012-02-21 09:39:19 +0100

    Initial Commit

---
+++ src/TestClass.java
@@ -0,0 +1,13 @@
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Git is fun");
+	}
+
+}
