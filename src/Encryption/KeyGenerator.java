7d45e35a2673de4d1d7906fb0550c9b8b7e4cea2
Frederic Jacobs <freeric.jacobs@mac.com>
2012-02-28 09:20:25 +0100

    Additions

--- src/Encryption/KeyGenerator.java
+++ src/Encryption/KeyGenerator.java
@@ -3,11 +3,15 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 /** Description of KeyGenerator 
  * @author Frederic Jacobs
  * @author Hantao Zhao 
  * @version 1.0
  */
+=======
+
+>>>>>>> adf19a2a5f8569faf688a6790f9c25f0a35f9c66
 =======
 
 >>>>>>> adf19a2a5f8569faf688a6790f9c25f0a35f9c66
