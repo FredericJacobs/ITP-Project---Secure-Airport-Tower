00836317973198695a5bd1c34ff3878c3d89affd
Frederic Jacobs <freeric.jacobs@mac.com>
2012-03-20 10:17:04 +0100

    Adding the Twitter

---
+++ src/main/MainClass.java
@@ -0,0 +1,13 @@
+package main;
+
+import twitter.*;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SendTweet.publish("HelloWorld");
+	}
+
+}
