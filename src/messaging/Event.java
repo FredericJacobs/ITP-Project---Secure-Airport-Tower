eeaa3dd5c2549cf0a91c4529930aaab5cb6c6b8b
hantao <zhthantao@hotmail.com>
2012-04-20 21:26:05 +0200

    Warning checked

--- src/messaging/Event.java
+++ src/messaging/Event.java
@@ -3,21 +3,50 @@ package messaging;
 import java.util.*;
 import messaging.messages.Message;
 
-@SuppressWarnings({ "rawtypes", "serial" })
-public class Event extends Vector{
-	@SuppressWarnings("unused")
+@SuppressWarnings({ "serial", "rawtypes" })
+public class Event extends Vector {
 	private Message message;
-	@SuppressWarnings("unused")
 	private String source;
-	@SuppressWarnings("unused")
 	private String destination;
-	@SuppressWarnings("unused")
 	private Date date;
+
 	public Event(Message message, String source, String destination) {
 		super();
 		this.message = message;
 		this.source = source;
 		this.destination = destination;
 		date = new Date();
+	}
+
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
 	}
 }
